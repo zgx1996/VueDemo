0fc6301f5c39e60201d9f2b6adecec24bc43858f
17305 <guoxi.zhang@hand-china.com>
2019-02-15 20:21:48 +0800

    init project

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+
+
+import { Header } from 'mint-ui';
+Vue.component(Header.name, Header)
+import './lib/mui/css/mui.min.css'
+
+import app from './App.vue'
+
+
+var vm = new Vue({
+  el:"#app",
+  data:{},
+  render : c => c(app)
+})
